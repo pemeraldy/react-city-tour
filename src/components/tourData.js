658b1413b2cae931e6488fb5736a6fc4ca3ddb98
pemeraldy <pemeraldy@gmail.com>
2019-04-03 04:09:29 +0100

    changedthe cities to places in kano

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -1,37 +1,37 @@
 export const tourData =[
     {
         id: 1,
-        city:"Kano Wall",
-        img: "./img/cityOne.jpg",
-        name: "new york bridge tour",
-        info: "Lorem Ipsum: usage. Lorem ipsum is a pseudo-Latin text used in web design, typography, "
+        city:"Kano",
+        img: "./img/gidanmakama.jpg",
+        name: "Gidan Makama",
+        info: "This was built in 1953 to preserve the rich history of Kano. It is notable as the home of past Emir's of Kano. "
     },
     {
         id: 2,
-        city:"paris",
-        img: "./img/cityTwo.jpg",
-        name: "paris light tour",
-        info: "Lorem Ipsum: usage. Lorem ipsum is a pseudo-Latin text used in web design, typography, "
+        city:"Kano",
+        img: "./img/kanowall.jpg",
+        name: "Kano city wall",
+        info: "This was built in 1095 AD–1134 AD. It is notable as a site that once protected citizens and inhabitants of Kano against external attack."
     },
     {
         id: 3,
-        city:"london",
-        img: "./img/cityThree.jpg",
-        name: "london royal palace",
-        info: "Lorem Ipsum: usage. Lorem ipsum is a pseudo-Latin text used in web design, typography, "
+        city:"Kano",
+        img: "./img/kofarmatad.jpg",
+        name: "Kofar Mata-Dyeing Pit",
+        info: "The Kofar Mata Pits are a part of the history of the ancient city of Kano. These pits that date back 5 centuries ago, during the golden age of Kano when it rivaled the riches of Timbuktu, gave Kano its place as a top textile manufacturer"
     },
     {
         id: 4,
-        city:"tokyo",
-        img: "./img/cityFour.jpg",
-        name: "Tokyo sushi tour",
-        info: "Lorem Ipsum: usage. Lorem ipsum is a pseudo-Latin text used in web design, typography, "
+        city:"Kano",
+        img: "./img/adobaye.jpg",
+        name: "Ado BayeroMall",
+        info: "People of Kano like to mix business with pleasure and no better place to do it as Nigeria's largest mall, Ado Bayero Mall. "
     },
     {
         id: 4,
-        city:"tokyo",
-        img: "./img/cityThree.jpg",
-        name: "Tokyo sushi tour",
-        info: "Lorem Ipsum: usage. Lorem ipsum is a pseudo-Latin text used in web design, typography, "
+        city:"Kano",
+        img: "./img/poloclub.jpg",
+        name: "Kano Polo Club",
+        info: "Polo is a game where man and horse come together to display prowess in the field game. The club is famous for its wins and tournaments. "
     },
 ]
